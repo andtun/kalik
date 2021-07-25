d20b850105538da83ef228f005ad66f30129747e
Andrey Tunyatkin <andtun@yandex.ru>
2021-07-25 14:57:42 +0300

    initial

---
+++ app.py
@@ -0,0 +1,24 @@
+# This Python file uses the following encoding: utf-8
+
+import markovify
+from bottle import *
+
+with open("kalik.txt", encoding='utf8') as f:
+    text = f.read()
+
+text_model_2 = markovify.Text(text, state_size=2, well_formed=False)
+
+def get_kalik():
+    res = None
+    while res is None:
+        res = text_model_2.make_sentence(min_words=35, max_words=100, tries=100)
+    return res
+
+
+@get("/")
+def main():
+    return gen_kalik()
+
+
+# run the server
+run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
