9ac5e4f6ab8fffc8dd8db04b3c395e19c0b8c449
Andrey Tunyatkin <andtun@yandex.ru>
2021-07-26 22:38:51 +0300

    chat bot

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # This Python file uses the following encoding: utf-8
 
+import vk_api
 import markovify
 from bottle import *
 
@@ -8,6 +9,11 @@ with open("kalik.txt", encoding='utf8') as f:
 
 easy = markovify.Text(text, state_size=2, well_formed=False)
 hard = markovify.Text(text, state_size=3, well_formed=False)
+
+vk = vk_api.VkApi(token='4d0e510aa01f5bf9cb5e68d8620939d03f11b939347c58acdff5f123458e662b0eafae16f92f3038aa717') #Авторизоваться как сообщество
+
+def write_msg(chat_id, s):
+    vk.method('messages.send', {'chat_id':1,'message':s, 'random_id':hash(s)})
 
 def gen_kalik(model, minw=35):
     res = None
@@ -23,6 +29,18 @@ def main():
 @get("/favicon.ico")
 def main_ico():
     return static_file('favicon.ico', root='.')
+
+@get("/342")
+def my_group():
+    msg = gen_kalik(easy)
+    write_msg(2, msg)
+    return 0
+
+@get("/342hard")
+def my_group():
+    msg = gen_kalik(hard, minw=25)
+    write_msg(2, msg)
+    return 0
 
 @post("/")
 def new_kalik_text():
